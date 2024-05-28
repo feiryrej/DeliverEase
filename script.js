b0bbb2a2d1fd7124476258214c9dd929a67ea1d0
Kyla <kflmorcillos@gmail.com>
2024-05-29 01:55:53 +0800

    Display map on html using Maps JavaScript API

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+let map;
 
+async function initMap() {
+  const { Map } = await google.maps.importLibrary("maps");
+
+  map = new Map(document.getElementById("map"), {
+    center: { lat: 14.600155451725184, lng: 121.01288018643108 },
+    zoom: 19,
+  });
+}
+
+initMap();
