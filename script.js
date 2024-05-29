e15127ae3be01fdcd21e16adf35352a2c2d8aaea
Kyla <kflmorcillos@gmail.com>
2024-05-29 09:52:39 +0800

    Try adding a marker

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
 let map;
+let marker;
 
+// initialize and add the map
 async function initMap() {
-  const { Map } = await google.maps.importLibrary("maps");
+	const { Map } = await google.maps.importLibrary("maps");
+	const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
 
-  map = new Map(document.getElementById("map"), {
-    center: { lat: 14.600155451725184, lng: 121.01288018643108 },
-    zoom: 19,
-  });
+	// display a map on the webpage
+	map = new Map(document.getElementById("map"), {
+		center: { lat: 14.600155451725184, lng: 121.01288018643108 },
+		zoom: 19,
+		mapId: '26050be015b4cb2d'
+	});
+	
+	marker = new AdvancedMarkerElement({
+		map,
+		position: { lat: 14.600155451725184, lng: 121.01288018643108}
+	});
 }
 
 initMap();
