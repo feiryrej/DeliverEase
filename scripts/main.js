d5e6cdf5eeba581da75d9f66622e58a42ccc4e03
Kyla <kflmorcillos@gmail.com>
2024-05-29 09:58:03 +0800

    Improve file structure

--- scripts/main.js
+++ scripts/main.js
@@ -1,22 +1,5 @@
-let map;
-let marker;
-
-// initialize and add the map
-async function initMap() {
-	const { Map } = await google.maps.importLibrary("maps");
-	const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
-
-	// display a map on the webpage
-	map = new Map(document.getElementById("map"), {
-		center: { lat: 14.600155451725184, lng: 121.01288018643108 },
-		zoom: 19,
-		mapId: '26050be015b4cb2d'
-	});
-	
-	marker = new AdvancedMarkerElement({
-		map,
-		position: { lat: 14.600155451725184, lng: 121.01288018643108}
-	});
+async function main() {
+	await initMap();
 }
 
-initMap();
+main();
