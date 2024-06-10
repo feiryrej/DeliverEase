573232dddd5edaa3114d4f0e67dfc7b348218958
Kyla <kflmorcillos@gmail.com>
2024-06-10 18:01:53 +0800

    Fix main

--- scripts/main.js
+++ scripts/main.js
@@ -13,11 +13,10 @@ async function main() {
 	animatePreloaderText();
 
 	const defaultLocation = "PUP, Anonas, Santa Mesa, Manila, Metro Manila, Philippines";
-
-	await initMap(defaultLocation);
-
 	const deliveriesDisplay = document.querySelector(".delivery-list");
 	deliveries = new Deliveries(deliveriesDisplay);
+
+	await initMap(defaultLocation);
 	deliveries.display();
 }
 
