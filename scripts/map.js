fe2296e1986311693cc5fa1d60a0c6d855173926
Kyla <kflmorcillos@gmail.com>
2024-06-10 15:21:31 +0800

    Implement Google Maps search box

--- scripts/map.js
+++ scripts/map.js
@@ -17,6 +17,8 @@ async function initMap() {
 		mapId: "26050be015b4cb2d"
 	});
 
+	setupSearchBox(map);
+
 	for (const pin of pins) {
 		const p = document.createElement("p");
 		p.textContent = pin[0];
@@ -49,9 +51,73 @@ async function initMap() {
 		.catch(error => {
 			console.error("Error fetching intersections:", error);
 		});
-	
+
 	// Initialize Google Maps API's Directions Service and Renderer
 	directionsService = new google.maps.DirectionsService();
 	directionsRenderer = new google.maps.DirectionsRenderer();
 	directionsRenderer.setMap(map);
+}
+
+function setupSearchBox(map) {
+	// Create the search box and link it to the UI element.
+	const input = document.getElementById("source-input");
+	const searchBox = new google.maps.places.SearchBox(input);
+
+	// Bias the SearchBox results towards current map's viewport.
+	map.addListener("bounds_changed", () => {
+		searchBox.setBounds(map.getBounds());
+	});
+
+	let markers = [];
+
+	// Listen for the event fired when the user selects a prediction and retrieve
+	// more details for that place.
+	searchBox.addListener("places_changed", () => {
+		const places = searchBox.getPlaces();
+
+		if (places.length == 0) {
+		return;
+		}
+
+		// Clear out the old markers.
+		markers.forEach((marker) => {
+		marker.setMap(null);
+		});
+		markers = [];
+
+		// For each place, get the icon, name and location.
+		const bounds = new google.maps.LatLngBounds();
+
+		places.forEach((place) => {
+		if (!place.geometry || !place.geometry.location) {
+			console.log("Returned place contains no geometry");
+			return;
+		}
+
+		const icon = {
+			url: place.icon,
+			size: new google.maps.Size(71, 71),
+			origin: new google.maps.Point(0, 0),
+			anchor: new google.maps.Point(17, 34),
+			scaledSize: new google.maps.Size(25, 25),
+		};
+
+		// Create a marker for each place.
+		markers.push(
+			new google.maps.Marker({
+			map,
+			icon,
+			title: place.name,
+			position: place.geometry.location,
+			}),
+		);
+		if (place.geometry.viewport) {
+			// Only geocodes have viewport.
+			bounds.union(place.geometry.viewport);
+		} else {
+			bounds.extend(place.geometry.location);
+		}
+		});
+		map.fitBounds(bounds);
+	});
 }
