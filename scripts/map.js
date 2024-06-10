377a254d8fb685c4f5743b739c30897ccd757fb2
Kyla <kflmorcillos@gmail.com>
2024-06-10 15:46:39 +0800

    Remove pins + imporive initMap

--- scripts/map.js
+++ scripts/map.js
@@ -1,10 +1,5 @@
 let map, directionsService, directionsRenderer;
 
-const pins = [
-	["1", 14.602415996238568, 121.01339990672079],
-	["2", 14.602535392412314, 121.01306194839161],
-];
-
 async function getCoordinates(address) {
 	const geocoder = new google.maps.Geocoder();
 	const res = await geocoder.geocode({ "address": address });
@@ -19,52 +14,26 @@ async function initMap(address) {
 	const { Map } = await google.maps.importLibrary("maps");
 	const { AdvancedMarkerElement, PinElement } = await google.maps.importLibrary("marker");
 
+	const source = await getCoordinates(address);
+
 	// Display a map on the webpage
 	map = new Map(document.getElementById("map"), {
-		center: await getCoordinates(address),
+		center: source,
 		zoom: 18,
 		mapId: "26050be015b4cb2d"
 	});
-
-	setupSearchBox(map);
-
-	for (const pin of pins) {
-		const p = document.createElement("p");
-		p.textContent = pin[0];
-		p.classList.add("pin-text");
-
-		new AdvancedMarkerElement({
-			map,
-			position: { lat: pin[1], lng: pin[2] },
-			content: new PinElement({glyph: p}).element,
-		});
-	}
-
-	// Fetch intersections and add markers
-	fetchIntersections()
-		.then(async intersections => {
-			intersections.forEach(intersection => {
-				const p = document.createElement("p");
-				p.textContent = `Intersection at (${intersection.lat}, ${intersection.lon})`;
-				p.classList.add("pin-text");
-
-				new AdvancedMarkerElement({
-					map,
-					position: { lat: intersection.lat, lng: intersection.lon },
-				});
-			});
-
-			// Display route using A* algorithm
-			await displayRoute(intersections);
-		})
-		.catch(error => {
-			console.error("Error fetching intersections:", error);
-		});
 
 	// Initialize Google Maps API's Directions Service and Renderer
 	directionsService = new google.maps.DirectionsService();
 	directionsRenderer = new google.maps.DirectionsRenderer();
 	directionsRenderer.setMap(map);
+
+	setupSearchBox(map);
+
+	const intersections = await fetchIntersections();
+
+	// Display route using A* algorithm
+	await displayRoute(intersections);
 }
 
 function setupSearchBox(map) {
