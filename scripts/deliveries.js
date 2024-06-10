2a47e457ce21505c8a6f4f849511827c59809cf3
Kyla <kflmorcillos@gmail.com>
2024-06-10 12:06:06 +0800

    Implement Deliveries

---
+++ scripts/deliveries.js
@@ -0,0 +1,63 @@
+const orders = [
+    {
+        "order_id": "2406069U1PVRCM",
+        "address": {
+            "street": "790-872 Roxas, Sampaloc, Maynila, 1008 Kalakhang Maynila",
+            "city_province": "Manila, Metro Manila"
+        },
+        "coordinates": {
+            "latitude": 14.609489,
+            "longitude": 121.009139
+        }
+    },
+    {
+        "order_id": "2406069SNCT2Q6",
+        "address": {
+            "street": "898-994 Bohol, Sampaloc, Maynila, 1008 Kalakhang Maynila",
+            "city_province": "Manila, Metro Manila"
+        },
+        "coordinates": {
+            "latitude": 14.60968045029885,
+            "longitude": 121.00722152922441
+        }
+    },
+    {
+        "order_id": "240528H1NP8TQS",
+        "address": {
+            "street": "Unang Hakbang, Lungsod Quezon, Kalakhang Maynila",
+            "city_province": "Quezon City, Metro Manila"
+        },
+        "coordinates": {
+            "latitude": 14.609970,
+            "longitude": 121.011379
+        }
+    }
+];
+
+class Deliveries {
+    display(element) {
+
+    }
+
+    getDefaultDeliveries() {
+        const order_ids = ["2406069U1PVRCM", "2406069SNCT2Q6"];
+        const res = [];
+
+        for (const order of orders) {
+            if (order_ids.includes(order["order_id"])) {
+                res.push(order);
+            }
+        }
+
+        return res;
+    }
+
+    getDeliveries() {
+        return JSON.parse(localStorage.getItem("deliveries"))
+            || this.getDefaultDeliveries();
+    }
+
+    addDelivery() {
+        const deliveries = this.getDeliveries();
+    }
+}
--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,11 @@
+let deliveries;
+
 async function main() {
 	await initMap();
+	deliveries = new Deliveries();
+
+	const deliveriesDisplay = document.querySelector(".delivery-list");
+	deliveries.display(deliveriesDisplay);
 }
 
 main();
