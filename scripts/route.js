9fa6db3a89d996cb9f00e11c519e7f666c3ce515
Kyla <kflmorcillos@gmail.com>
2024-06-13 17:34:48 +0800

    Move haversine function to route.js

--- scripts/route.js
+++ scripts/route.js
@@ -57,3 +57,15 @@ async function displayRoute(source) {
 		}
 	});
 }
+
+function haversine(lat1, lon1, lat2, lon2) {
+	// Haversine formula implementation
+	const r = 6371; // km
+	const p = Math.PI / 180;
+
+	const a = 0.5 - Math.cos((lat2 - lat1) * p) / 2
+					+ Math.cos(lat1 * p) * Math.cos(lat2 * p) *
+					(1 - Math.cos((lon2 - lon1) * p)) / 2;
+
+	return 2 * r * Math.asin(Math.sqrt(a));
+}
