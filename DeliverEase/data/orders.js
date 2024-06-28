4f5984e5aea75bd5df5fa151c23ad08b37c69b17
dotBisch <franzivan.devilla@gmail.com>
2024-06-28 18:14:53 +0800

    Update delivery data

--- DeliverEase/data/orders.js
+++ DeliverEase/data/orders.js
@@ -2,24 +2,24 @@ const orders = {
     "2406069U1PVRCM": {
         "order_id": "2406069U1PVRCM",
         "address": {
-            "street": "J265+9RX Visayan Avenue, Galas, Quezon City, 1106 Metro Manila",
-            "city_province": "Quezon City, Metro Manila"
+            "street": "2158 Mindanao Ave, Sampaloc, Manila, 1008 Metro Manila",
+            "city_province": "Manila, Metro Manila"
         },
         "coordinates": {
-            "latitude": 14.610931481418678,
-            "longitude": 121.00962461983127
-        }
+            "latitude": 14.607870120803339,
+            "longitude": 121.00877446429824
+        },   
     },
     "240528H1NP8TQS": {
         "order_id": "240528H1NP8TQS",
         "address": {
-            "street": "991-881 Bataan, Sampaloc, Maynila, 1008 Kalakhang Maynila",
+            "street": "2101b Samar Street, Sampaloc, Maynila, 1008 Kalakhang Maynila",
             "city_province": "Manila, Metro Manila"
         },
         "coordinates": {
-            "latitude": 14.611014536416397,
-            "longitude": 121.00833715952957
-        }
+            "latitude": 14.608037710721195,
+            "longitude": 121.00699577721888
+        }  
     },
     "24052340GFPN7G": {
         "order_id": "24052340GFPN7G",
@@ -28,195 +28,195 @@ const orders = {
             "city_province": "Manila, Metro Manila"
         },
         "coordinates": {
-            "latitude": 14.609734440415629,
-            "longitude": 121.00939597664525
-        }
+            "latitude": 14.607706565787252,
+            "longitude": 121.00746060341531
+        }  
     },
     "240506K4VFWEK5": {
         "order_id": "240506K4VFWEK5",
         "address": {
-            "street": "2117 Visayan Avenue, Sampaloc, Manila, 1008 Metro Manila",
+            "street": "898-994 Bohol, Sampaloc, Maynila, 1008 Kalakhang Maynila",
             "city_province": "Manila, Metro Manila"
         },
         "coordinates": {
-            "latitude": 14.608687847617677,
-            "longitude": 121.00785014749086
-        }
+            "latitude": 14.609334797273833,
+            "longitude": 121.00765534596299
+        }  
     },
     "240506K4KX9F6H": {
         "order_id": "240506K4KX9F6H",
         "address": {
-            "street": "756-798 Panay St, Sampaloc, Manila, Metro Manila",
+            "street": "884-990 Cebu, Sampaloc, Maynila, 1008 Kalakhang Maynila",
             "city_province": "Manila, Metro Manila"
         },
         "coordinates": {
-            "latitude": 14.609071569882474,
-            "longitude": 121.01031654564558
+            "latitude": 14.60989975876693, 
+            "longitude": 121.00809738438326
         }
     },
     "240424HXYQ5GJ2": {
         "order_id": "240424HXYQ5GJ2",
         "address": {
-            "street": "812-900 Negros, Sampaloc, Maynila, 1008 Kalakhang Maynila",
+            "street": "2-46 Union Civica, Maynila, Kalakhang Maynila",
             "city_province": "Manila, Metro Manila"
         },
         "coordinates": {
-            "latitude": 14.60703408984203,
-            "longitude": 121.00699574056995
-        }
+            "latitude": 14.613401756890637, 
+            "longitude": 121.00984218539386 
+        } 
     },
     "2404183QBAB6QT": {
         "order_id": "2404183QBAB6QT",
         "address": {
-            "street": "882-998 Mindoro, Sampaloc, Maynila, 1008 Kalakhang Maynila",
+            "street": "2114c Leyte Del Sur, Sampaloc, Maynila, 1008 Kalakhang Maynila",
             "city_province": "Manila, Metro Manila"
         },
         "coordinates": {
-            "latitude": 14.606611743229312,
-            "longitude": 121.00545464824579
+            "latitude": 14.609352944669665, 
+            "longitude": 121.00595052584761
         }
     },
     "240425N8AM7X09": {
         "order_id": "240425N8AM7X09",
         "address": {
-            "street": "",
-            "city_province": "Manila, Metro Manila"
+            "street": "80 Unang Hakbang, Lungsod Quezon, 1113 Kalakhang Maynila",
+            "city_province": "Quezon City, Metro Manila"
         },
         "coordinates": {
-            "latitude": 14.606353817355037,
-            "longitude": 121.00411346956557
+            "latitude": 14.610736352950648, 
+            "longitude": 121.01045140783793
         }
     },
     "2404196ERP2Q7T": {
         "order_id": "2404196ERP2Q7T",
         "address": {
-            "street": "894-986 Rosarito, Sampaloc, Maynila, 1008 Kalakhang Maynila",
+            "street": "923 Batanes St, Sampaloc, Manila, 1008 Metro Manila",
             "city_province": "Manila, Metro Manila"
         },
         "coordinates": {
-            "latitude": 14.608734121559147,
-            "longitude": 121.00551477487286
+            "latitude": 14.611056264399304, 
+            "longitude": 121.00885918939052
         }
     },
     "2404086CK36S6J": {
         "order_id": "2404086CK36S6J",
         "address": {
-            "street": "Leyte Del Sur, Sampaloc, Maynila, 1008 Kalakhang Maynila",
+            "street": "1014-1076 Cebu, Sampaloc, Maynila, 1008 Kalakhang Maynila",
             "city_province": "Manila, Metro Manila"
         },
         "coordinates": {
-            "latitude": 14.60876586993703,
-            "longitude": 121.00679449947349
+            "latitude": 14.611015182339266, 
+            "longitude": 121.00659774428497
         }
     },
     "2404170BXAY2CA": {
         "order_id": "2404170BXAY2CA",
         "address": {
-            "street": "890-1010 Leyte St, Sampaloc, Manila, 1008 Metro Manila",
+            "street": "2323-2319 Luzon Ave, Sampaloc, Manila, 1008 Metro Manila",
             "city_province": "Manila, Metro Manila"
         },
         "coordinates": {
-            "latitude": 14.60892968985143,
-            "longitude": 121.00694840231112
+            "latitude": 14.611594472470086, 
+            "longitude": 121.00809529527533
         }
     },
     "2404063AEH0P2Q": {
         "order_id": "2404063AEH0P2Q",
         "address": {
-            "street": "",
-            "city_province": "Manila, Metro Manila"
+            "street": "201-177 Batanes St, Sampaloc, Manila, Metro Manila",
+            "city_province": "Quezon City, Metro Manila"
         },
         "coordinates": {
-            "latitude": 14.608497800722839,
-            "longitude": 121.00411659009849
+            "latitude": 14.609465922645876, 
+            "longitude": 121.01090662063739
         }
     },
     "2404074JQM6CAN": {
         "order_id": "2404074JQM6CAN",
         "address": {
-            "street": "Masbate, Sampaloc, Maynila, 1008 Kalakhang Maynila",
+            "street": "794-797 Mindanao Ave, Sampaloc, Manila, 1008 Metro Manila",
             "city_province": "Manila, Metro Manila"
         },
         "coordinates": {
-            "latitude": 14.610139146610361,
-            "longitude": 121.00484481334206
+            "latitude": 14.60945989036091, 
+            "longitude": 121.01011654281938
         }
     },
     "240405VP8VDM0E": {
         "order_id": "240405VP8VDM0E",
         "address": {
-            "street": "",
-            "city_province": "Manila, Metro Manila"
+            "street": "Liberation, Lungsod Quezon, Kalakhang Maynila",
+            "city_province": "Quezon City, Metro Manila"
         },
         "coordinates": {
-            "latitude": 14.61009761895002,
-            "longitude": 121.00675454612289
+            "latitude": 14.611286637196423, 
+            "longitude": 121.01161822697894
         }
     },
     "240330DB2U0UWK": {
         "order_id": "240330DB2U0UWK",
         "address": {
-            "street": "Unit C 2180 Luzon Avenue, corner Bohol, Sampaloc, 1008 Metro Manila",
+            "street": "800-882 Panay St, Sampaloc, Manila, 1008 Metro Manila",
             "city_province": "Manila, Metro Manila"
         },
         "coordinates": {
-            "latitude": 14.611343445346966,
-            "longitude": 121.0062824773456
+            "latitude": 14.609422625304974, 
+            "longitude": 121.00980798119298
         }
     },
     "2403288QRTN7F0": {
         "order_id": "2403288QRTN7F0",
         "address": {
-            "street": "22 Unang Hakbang, Lungsod Quezon, 1113 Kalakhang Maynila",
-            "city_province": "Quezon City, Metro Manila"
+            "street": "792 Roxas, Sampaloc, Maynila, 1008 Kalakhang Maynila",
+            "city_province": "Manila, Metro Manila"
         },
         "coordinates": {
-            "latitude": 14.61238162861664,
-            "longitude": 121.00810637943967
+            "latitude": 14.609115147027214, 
+            "longitude": 121.00965486760818
         }
     },
     "2403288NAUTFAX": {
         "order_id": "2403288NAUTFAX",
         "address": {
-            "street": "89-63 Bustamante, Maynila, Kalakhang Maynila",
-            "city_province": "Quezon City, Metro Manila"
+            "street": "871-789 Cebu, Sampaloc, Maynila, 1008 Kalakhang Maynila",
+            "city_province": "Manila, Metro Manila"
         },
         "coordinates": {
-            "latitude": 14.61088664315468,
-            "longitude": 121.01065984237133
+            "latitude": 14.609466030045485, 
+            "longitude": 121.00863372461585
         }
     },
     "240320J1X86MJM": {
         "order_id": "240320J1X86MJM",
         "address": {
-            "street": "115-87 Madiasas, Lungsod Quezon, Kalakhang Maynila",
+            "street": "861-803 Batanes St, Sampaloc, Manila, 1008 Metro Manila",
             "city_province": "Quezon City, Metro Manila"
         },
         "coordinates": {
-            "latitude": 14.61061671414026,
-            "longitude": 121.01246228679369
+            "latitude": 14.61050439774156,
+            "longitude": 121.00958055165344
         }
     },
     "2403154QC3E3VE": {
         "order_id": "2403154QC3E3VE",
         "address": {
-            "street": "81-55 Cuatro De Julio, Maynila, Kalakhang Maynila",
+            "street": "861-803 Batanes St, Sampaloc, Manila, 1008 Metro Manila",
             "city_province": "Quezon City, Metro Manila"
         },
         "coordinates": {
-            "latitude": 14.611987119551873,
-            "longitude": 121.01108899580521
+            "latitude": 14.610355541911392, 
+            "longitude": 121.00973696912544
         }
     },
     "240306BQQ4374A": {
         "order_id": "240306BQQ4374A",
         "address": {
-            "street": "37-27 San Isidro St, Quezon City, Metro Manila",
-            "city_province": "Quezon City, Metro Manila"
+            "street": "989-881 Panay St, Sampaloc, Manila, 1008 Metro Manila",
+            "city_province": "Manila, Metro Manila"
         },
         "coordinates": {
-            "latitude": 14.613108353987345,
-            "longitude": 121.01065984237133
+            "latitude": 14.610981957920876, 
+            "longitude": 121.00780442758553
         }
     }
 };
